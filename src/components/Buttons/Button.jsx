8e208413bfcb70ad664e5b31dff81310396211cd
Oğuz Sevsel <oguzsevsel@gmail.com>
2025-02-20 05:14:10 +0300

    Initial Commit

---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Button = (onClick, isIcon, icon, buttonText, rounding, className) => {
+
+    return (
+        <button className={className} style={{borderRadius: rounding}} onClick={onClick} >
+            {isIcon ? <icon/> : buttonText}
+        </button>
+    )
+    
+}
+
+export default Button;
